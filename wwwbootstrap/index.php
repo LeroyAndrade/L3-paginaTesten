f36a44c23bdc50cd9f43add56ce71201e3c2868f
LeroyAndrade <30168@ma-web.nl>
2022-04-29 09:59:52 +0200

    Eerste push
    Eerste push

---
+++ wwwbootstrap/index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+  <head>
+      
+    <meta charset="utf-8"><meta name="description" content="HW">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content=" ">
+    <meta name="author" content="Leroy Andrade">
+
+    <title>Opdracht 5</title>
+<!--Bootstrap 5--><link rel="stylesheet" type="text/css" href="./opmaak/style.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <link rel="stylesheet" type="text/css" href="./opmaak/style.css">
+
+  </head>
+
+  <body lang="nl">
+
+  <div id = "headerBackground" class="container-fluid sticky-top">
+
+      <div class="row">
+          <div class="col-2">
+              <div id="placeholderImage"><img src="./img/main/placeholderImage.svg" alt="test"></div>
+          </div>
+
+          <div class="col-8">&nbsp;</div>
+
+          <div class="col-2">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hamburgerButton" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+          </div>
+      </div>
+
+
+
+      <div class="row align-items-start">
+          <div class="col">
+              <hr>
+          </div>
+      </div>
+
+      <div class="row align-items-end">
+          <div class="col">
+              One of three columns
+          </div>
+          <div class="col">
+              One of three columns
+          </div>
+          <div class="col">
+              One of three columns
+          </div>
+      </div>
+  </div>
+
+
+
+
+    <div class="wrapper">
+        <br><br><br><br><br>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    Column2
+                </div>
+                <div class="col">
+                    Column2
+                </div>
+                <div class="col">
+                    Column2
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+ </body>
+
+
+</html>
---
+++ wwwflexBox/template.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+ <meta charset="utf-8">
+ <meta name="description" content="HW">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta name="keywords" content=" ">
+ <meta name="author" content="Leroy Andrade">
+
+ <title>Hogeschool Utrecht</title>
+ <meta name="author" content="Hier komt jouw naam">
+ <link rel="stylesheet" type="text/css" href="./opmaak/template.css?v6">
+</head>
+
+ <body lang="nl">
+
+ <div id="headerContainer">
+  <section class="top">
+   <div id="placeholderImage"><img src="./img/main/placeholderImage.svg" alt="test"></div>
+   <!--      <span><img src="https://via.placeholder.com/70x46" alt="test"></span>-->
+
+   <div> &nbsp; </div>
+
+   <div>
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hamburgerButton" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+     <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+    </svg>
+    <div>
+     <!--      &#8801;-->
+  </section>
+  <hr>
+ </div>
+ <!--  <hr class="hrOne">-->
+
+
+ <header>
+  <article class="headerMainTekst">Overzicht</article>
+     <br>
+  <select id = "mainSelect" name = "country">
+   <option value = "java" selected disabled>filteren &#46;&#46;</option>
+   <option value = "java">Ontwerpen&#44; ontwikkelen en maken van producten</option>
+   <option value = "ruby">Media vormgeven &#38; Ict</option>
+   <option value = "oracle">Constructies en Bouwwerken</option>
+   <option value = "tableau">Materiaaltechnologie</option>
+   <option value = "tableau">Elektrische systemen &#44; Elektronica en Robotica</option>
+   <option value = "tableau">Traditional &#38; Digital Fabrication</option>
+   <option value = "tableau">Circulair Skills</option>
+  </select>
+ </header>
+
+ <main>
+  <form action="#" type="POST" enctype="application/x-www-form-urlencoded"></form>
+
+  <span></span>
+
+ </main>
+
+ </body>
+</html>
